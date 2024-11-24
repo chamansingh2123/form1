5e03fbc60d87ef3ca913bdf1e6c7b9bd4d3f24e2
chamansingh2123 <chamansingh5464@gmail.com>
2024-11-24 15:32:50 +0530

    Add files via upload

---
+++ BOOTSTRAP/BOOTSTREP/first.js
@@ -0,0 +1,42 @@
+//variable Equal to
+var a=5;
+var b=8;
+var a=b;
+// var Equal to=false
+console.log(Equal)
+//variable declaration for Not Equl to 
+var a=3;
+var b=9;
+var a=b;
+// var Not Equal to= true;
+// console.log(Not Equal to)
+//variable declaration for Greater then 
+var a=5;
+var b=6;
+
+console.log(multiplication)
+//variable declaration for division
+var a=7;
+var b=4;
+var division=a/b;
+console.log(division)
+//variable declaration for reminder
+var a=15;
+var b=3;
+var reminder=a%b;
+console.log(reminder)
+//variable declaration for increment
+var a=9;
+var b=5;
+var increment=a++;
+console.log(increment)
+//variable decleration for decrement
+var a=8;
+var b=7;
+var decrement=a--;
+console.log(decrement)
+//variable decleration for exponentiation
+var a=12;
+var b=15;
+var exponentiation=a**2;
+console.log(exponentiation)
---
+++ BOOTSTRAP/BOOTSTREP/operators.js
@@ -0,0 +1,40 @@
+//variable declaration
+var a=5;
+var b=8;
+var sum=a+b;
+console.log(sum)
+//variable declaration for substraction
+var a=3;
+var b=9;
+var substraction=a-b;
+console.log(substraction)
+//variable declaration for multiplication
+var a=5;
+var b=6;
+var multiplication=a*b;
+console.log(multiplication)
+//variable declaration for division
+var a=7;
+var b=4;
+var division=a/b;
+console.log(division)
+//variable declaration for reminder
+var a=15;
+var b=3;
+var reminder=a%b;
+console.log(reminder)
+//variable declaration for increment
+var a=9;
+var b=5;
+var increment=a++;
+console.log(increment)
+//variable decleration for decrement
+var a=8;
+var b=7;
+var decrement=a--;
+console.log(decrement)
+//variable decleration for exponentiation
+var a=12;
+var b=15;
+var exponentiation=a**2;
+console.log(exponentiation)
